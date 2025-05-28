b073c51114f32409c220b4b1f2c550bf658088a0
Alexis de Lattre <alexis.delattre@akretion.com>
2025-05-29 01:04:57 +0200

    [FIX] account_payment_sepa_base: fix website in manifest

--- account_payment_sepa_base/__manifest__.py
+++ account_payment_sepa_base/__manifest__.py
@@ -10,7 +10,7 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Akretion, Noviat, Tecnativa, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/bank-payment",
+    "website": "https://github.com/OCA/bank-payment-alternative",
     "category": "Hidden",
     "depends": ["account_payment_batch_oca"],
     "excludes": ["account_banking_pain_base"],
