c7f643c08eda1e4bd928f8c2aba7e3f9b412b21d
Alexis de Lattre <alexis.delattre@akretion.com>
2025-07-02 17:38:22 +0200

    [IMP] Continue code cleanup in SEPA XML file generation
    Reduce number of args in methods
    Improve variable names to avoid mis-leading names
    Remove @api.model where it's not appropriate
    Add raise_if_oversized=True in _prepare_field() where appropriate

--- account_payment_sepa_credit_transfer/models/account_payment_order.py
+++ account_payment_sepa_credit_transfer/models/account_payment_order.py
@@ -65,8 +65,8 @@ class AccountPaymentOrder(models.Model):
         for lot in self.payment_lot_ids:
             # B. Payment info
             payment_info = lot._generate_start_payment_info_block(pain_root, gen_args)
-            self._generate_party_block(
-                payment_info, "Dbtr", "B", self.company_partner_bank_id, gen_args
+            self.company_partner_bank_id._generate_party_block(
+                payment_info, "B", gen_args
             )
             self._generate_charge_bearer(payment_info)
             for payment in lot.payment_ids:
@@ -84,12 +84,14 @@ class AccountPaymentOrder(models.Model):
                     payment_ident_val,
                     35,
                     gen_args,
+                    raise_if_oversized=True,
                 )
                 payment_identification.EndToEndId = self._prepare_field(
                     "End to End Identification",
                     payment_ident_val,
                     35,
                     gen_args,
+                    raise_if_oversized=True,
                 )
                 amount = objectify.SubElement(credit_transfer_transaction_info, "Amt")
                 amount.InstdAmt = payment.currency_id._pain_format(payment.amount)
@@ -105,11 +107,9 @@ class AccountPaymentOrder(models.Model):
                         )
                     )
 
-                self._generate_party_block(
+                payment.partner_bank_id._generate_party_block(
                     credit_transfer_transaction_info,
-                    "Cdtr",
                     "C",
-                    payment.partner_bank_id,
                     gen_args,
                     payment,
                 )
